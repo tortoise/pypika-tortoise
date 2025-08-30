8b5e9e62ce741ffced2915e48fb33c8b367ddaaa
Waket Zheng <waketzheng@gmail.com>
2025-08-30 15:42:47 +0800

    tests: fix poetry not found error

--- tests/test_poetry_add.py
+++ tests/test_poetry_add.py
@@ -1,3 +1,5 @@
+import shlex
+import shutil
 import subprocess  # nosec
 import sys
 from pathlib import Path
@@ -24,11 +26,21 @@ else:
             os.chdir(self._old_cwd.pop())
 
 
+def _run_shell(cmd: str, **kw) -> subprocess.CompletedProcess[str]:
+    return subprocess.run(shlex.split(cmd), **kw)  # nosec
+
+
 def test_added_by_poetry_v2(tmp_path: Path):
     lib = Path(__file__).parent.resolve().parent
+    py = "{}.{}".format(*sys.version_info)
+    poetry = "poetry"
+    if shutil.which(poetry) is None:
+        poetry = "uvx " + poetry
     with chdir(tmp_path):
         package = "foo"
-        subprocess.run(["poetry", "new", package])  # nosec
+        _run_shell(f"{poetry} new {package} --python=^{py}")  # nosec
         with chdir(package):
-            r = subprocess.run(["poetry", "add", lib])  # nosec
+            _run_shell(f"{poetry} config --local virtualenvs.in-project true")  # nosec
+            _run_shell(f"{poetry} env use {py}")  # nosec
+            r = _run_shell(f"{poetry} add {lib}")  # nosec
             assert r.returncode == 0
